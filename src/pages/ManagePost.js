4c5f8f5eea9740f898f01ab9a392b22cc3f97a94
txphan <txphan@cs.olemiss.edu>
2024-11-12 19:20:09 -0600

    fix

--- src/pages/ManagePost.js
+++ src/pages/ManagePost.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
